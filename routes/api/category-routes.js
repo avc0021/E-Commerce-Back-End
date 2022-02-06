f77e80991637a942da2f9dce0065bd7e67183c52
avc0021 <avc0021@gmail.com>
2022-02-05 20:54:53 -0600

    models done, index model is in progress

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -16,7 +16,7 @@ router.get('/', (req, res) => {
   })
   .then (dbCategoryData => res.json(dbCategoryData))
   .catch(err => {
-    console.log(err);
+    console.log(err)
     res.status(500).json(err);
   });
 });
